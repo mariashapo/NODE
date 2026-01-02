3fb2a035644b806c2f68a369e9f5e31636697451
mariashapo <mart99maria@gmail.com>
2026-01-02 18:20:54 +0000

    Modify JAX script to save more hyper-parameters.

--- src/training_convergence/training_conv_jax.py
+++ src/training_convergence/training_conv_jax.py
@@ -178,7 +178,7 @@ def main(argv=None):
             except Exception: pass
 
         if args.timing_only or not args.log:
-            results["train_loss"] = results_no_log.get("train_loss", getattr(trainer, "losses", None))
+            results["train_loss"] = results_no_log.get("train_loss")
             results["data_type"] = args.data_type
             results["pretrain"] = pretrain_value
             results["max_iter"] = args.max_iter
